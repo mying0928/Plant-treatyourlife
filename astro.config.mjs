5b9705c915a032a195e721aff8832defd56adbfb
yi-cheng <mying0928@gmail.com>
2025-11-21 20:17:56 +0800

    Fix: Correct base path for GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: 'https://mying0928.github.io',
-  base: '/Plant-Astro', // 我們將部署到新的儲存庫
+  site: 'https://mying0928.github.io/Plant-treatyourlife',
+  base: '/Plant-treatyourlife',
   integrations: [tailwind()]
 });
